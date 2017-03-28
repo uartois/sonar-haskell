a77ca50da588d75bdf7eb065d6c731550aaad9a3
mohamed_boumati <mohamed_boumati@ens.univ-artois.fr>
2017-03-28 09:26:42 +0200

    Begin

---
+++ src/main/java/fr/univartois/sonarhaskell/SonarHaskellPlugin.java
@@ -0,0 +1,12 @@
+package fr.univartois.sonarhaskell;
+
+import org.sonar.api.Plugin;
+
+public class SonarHaskellPlugin implements Plugin{
+
+	@Override
+	public void define(Context arg0) {
+		
+	}
+
+}
