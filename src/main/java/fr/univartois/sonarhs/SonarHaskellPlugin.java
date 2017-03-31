56be48cae0cda5a60229531205c45ab43f502d8e
mohamed_boumati <mohamed_boumati@ens.univ-artois.fr>
2017-03-31 12:51:58 +0200

    Update package s name

--- src/main/java/fr/univartois/sonarhs/SonarHaskellPlugin.java
+++ src/main/java/fr/univartois/sonarhs/SonarHaskellPlugin.java
@@ -1,11 +1,11 @@
-package fr.univartois.sonarhaskell;
+package fr.univartois.sonarhs;
 
 import org.sonar.api.Plugin;
 
 public class SonarHaskellPlugin implements Plugin{
 
 	@Override
-	public void define(Context arg0) {
+	public void define(Context context) {
 		
 	}
 
