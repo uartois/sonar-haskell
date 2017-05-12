452efa3fd65a06d080cea33b696f8a7f79f7156b
mohamed_boumati <mohamed_boumati@ens.univ-Artois.fr>
2017-05-12 17:06:55 +0200

    update tests

--- src/test/java/fr/univartois/sonarhs/HaskellLanguageTest.java
+++ src/test/java/fr/univartois/sonarhs/HaskellLanguageTest.java
@@ -1,5 +1,22 @@
 package fr.univartois.sonarhs;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
 public class HaskellLanguageTest {
  
+    private HaskellLanguage haskellLanguage;
+
+    @Before
+    public void setUp() {
+	haskellLanguage = new HaskellLanguage();
+    }
+
+    @Test
+    public void testSuffixes() {
+	assertThat(haskellLanguage.getFileSuffixes()).containsOnly("hs");
+    }
+    
 }
