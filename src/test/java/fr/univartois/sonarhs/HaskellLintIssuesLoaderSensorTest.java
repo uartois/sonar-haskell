d501aab7ca950c18fe68607f84acb55f1ba88822
mohamed_boumati <mohamed_boumati@ens.univ-Artois.fr>
2017-05-15 00:53:47 +0200

    fix Copyrights

--- src/test/java/fr/univartois/sonarhs/HaskellLintIssuesLoaderSensorTest.java
+++ src/test/java/fr/univartois/sonarhs/HaskellLintIssuesLoaderSensorTest.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Copyright 2017 - Université d'Artois
+ *  
+ * This file is part of SonarQube Haskell plugin (sonar-haskell).
+ *  
+ * Sonar-haskell is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *  
+ * Sonar-haskell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Sonar-haskell.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ * Contributors:
+ *              Mohamed Boutmati (mohamed_boumati@ens.univ-artois.fr)
+ *******************************************************************************/
 package fr.univartois.sonarhs;
 
 import org.junit.Test;
