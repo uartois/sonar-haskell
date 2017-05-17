8c9a5976c5d82d7eced2ca5d00f16e90c4b9a346
mohamed_boumati <mohamed_boumati@ens.univ-artois.fr>
2017-05-17 11:20:52 +0200

    fix Copyrights
    é -> e

--- src/test/java/fr/univartois/sonarhs/SonarHaskellPluginTest.java
+++ src/test/java/fr/univartois/sonarhs/SonarHaskellPluginTest.java
@@ -1,22 +1,22 @@
 /*******************************************************************************
- * Copyright 2017 - Université d'Artois
+ *  Copyright 2017 - Universite d'Artois
  *  
- * This file is part of SonarQube Haskell plugin (sonar-haskell).
+ *  This file is part of SonarQube Haskell plugin (sonar-haskell).
  *  
- * Sonar-haskell is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version.
+ *  Sonar-haskell is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *  
- * Sonar-haskell is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *  Sonar-haskell is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *  
- * You should have received a copy of the GNU Lesser General Public License
- * along with Sonar-haskell.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Sonar-haskell.  If not, see <http://www.gnu.org/licenses/>.
  *  
- * Contributors:
+ *  Contributors:
  *              Mohamed Boumati (mohamed_boumati@ens.univ-artois.fr)
  *******************************************************************************/
 package fr.univartois.sonarhs;
